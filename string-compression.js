9b1d1612fabd91fff19e5c2a2cea484284b77e20
Joycee Catamora Paragas <jcprgs262@gmail.com>
2016-08-04 03:05:17 +0800

    Added first code challenge file

---
+++ string-compression.js
@@ -0,0 +1,18 @@
+var items=[];
+function compress(str) {
+  var count = 0; fcount = 0, itemChar = 0;
+  for(var index = 0; index < str.length; index++) {
+    var char = str[index];
+    if(char == str[index+1]){
+      count += 1
+    }
+    else if(char != str[index+1]) {
+      fcount = count + 1
+      itemChar = fcount + String(char)
+      items.push(itemChar)
+      count = 0
+    }
+  }
+    var arrToStr = String(items);
+    return arrToStr.replace(/,/g,"")	
+}
