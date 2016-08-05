546b6c4388e7babd445cb1aa02f7d3f5b7fd621a
Joycee Catamora Paragas <jcprgs262@gmail.com>
2016-08-05 16:29:46 +0800

    Fixed tabs

--- ui-centric-question(javascript)/to-do.js
+++ ui-centric-question(javascript)/to-do.js
@@ -8,89 +8,81 @@ projTot = document.getElementById('gtotal'),
 li, items = [];
 
 function enterProjectName(e) {
-	e.which = e.which || e.keyCode;
-	if(e.which == 13) {
-		checkInputVal()
-		clearInputField()
-		countListUl()
-	}
+  e.which = e.which || e.keyCode;
+  if(e.which == 13) {
+    checkInputVal()
+    clearInputField()
+    countListUl()
+  }
 }
 
 function addInputValToList() {
-	countItemArray(input.value)
-	li = document.createElement("li");
-	li.innerHTML = input.value
-	ul.appendChild(li)
-	addDraggablePropsToLi()
+  countItemArray(input.value)
+  li = document.createElement("li");
+  li.innerHTML = input.value
+  ul.appendChild(li)
+  addDraggablePropsToLi()
 }
 
-
 function clearInputField() {
-	input.value = ""
+  input.value = ""
 }
 
 function checkInputVal() {
-	if (input.value == " " || input.value == null || input.value === "" ) {
-		warning.style.color = "red"
-		warning.style.fontSize = "16px"
-		warning.style.display = ""
-		warning.innerHTML = "PLEASE ENTER A VALID ITEM"	
-		setTimeout(function(){
-			warning.style.display = "none"
-		},2500);
-	}
-	else {
-		addInputValToList()
-	}	
+  if (input.value == " " || input.value == null || input.value === "" ) {
+    warning.style.color = "red"
+    warning.style.fontSize = "16px"
+    warning.style.display = ""
+    warning.innerHTML = "PLEASE ENTER A VALID ITEM"	
+    setTimeout(function(){
+      warning.style.display = "none"
+    },2500);
+  }
+  else {
+    addInputValToList()
+  }	
 }
 
 function countItemArray(item) {
-	items.push(item)
-	projTot.innerHTML = "" + items.length + "<br> Projects" + "" 
-	console.log(items.length)
+  items.push(item)
+  projTot.innerHTML = "" + items.length + "<br> Projects" + "" 
+  console.log(items.length)
 }
 
 function countListUl() {
-	var todo = document.getElementById("todo-ul").getElementsByTagName("li"),
-	inprog = document.getElementById("inprogress-ul").getElementsByTagName("li"),
-	done = document.getElementById("done-ul").getElementsByTagName("li");
-	todoCounter.innerHTML = todo.length + " Projects"
-	inprogCounter.innerHTML = inprog.length + " Projects"
-	doneCounter.innerHTML = done.length + " Projects"
+  var todo = document.getElementById("todo-ul").getElementsByTagName("li"),
+  inprog = document.getElementById("inprogress-ul").getElementsByTagName("li"),
+  done = document.getElementById("done-ul").getElementsByTagName("li");
+  todoCounter.innerHTML = todo.length + " Projects"
+  inprogCounter.innerHTML = inprog.length + " Projects"
+  doneCounter.innerHTML = done.length + " Projects"
 }
 
+//Inprogress
 function sortList(id) {
-	 var ul = id;
-	 console.log(ul)
-  
+  var ul = id;
+  console.log(ul)
 }
 
 //drag & drop events
 function addDraggablePropsToLi() {
-	li.setAttribute("id", input.length + input.value)
-	li.setAttribute("draggable", "true")
-	li.setAttribute("ondragstart", "dragStart(event)")
+  li.setAttribute("id", input.length + input.value)
+  li.setAttribute("draggable", "true")
+  li.setAttribute("ondragstart", "dragStart(event)")
 }
 
 function dragStart(e) {
-	event.dataTransfer.setData("Text", event.target.id);
-	console.log("hello")
+  event.dataTransfer.setData("Text", event.target.id);
+  console.log("hello")
 }
 
 function drop(e) {
-	e.preventDefault()
-	var data = event.dataTransfer.getData("Text");
+  e.preventDefault()
+  var data = event.dataTransfer.getData("Text");
   event.target.appendChild(document.getElementById(data));
-	countListUl()
+  countListUl()
 }
 
 function allowDrop(e) {
-	e.preventDefault()
+  e.preventDefault()
 }
-
-
-
-
-
-
-
