0a3a66e9ac247a490464ae8af467b8d3ae5a9e62
Joycee Catamora Paragas <jcprgs262@gmail.com>
2016-08-05 16:21:23 +0800

    Updated ui centric code challenge files

--- ui-centric-question(javascript)/to-do.js
+++ ui-centric-question(javascript)/to-do.js
@@ -2,30 +2,28 @@ var input = document.getElementById('ptext'),
 ul = document.getElementById('todo-ul'),
 warning = document.getElementById('warning'),
 todoCounter = document.getElementById('todo-counter'),
+inprogCounter = document.getElementById('inprog-counter'),
+doneCounter = document.getElementById('done-counter'),
+projTot = document.getElementById('gtotal'),
 li, items = [];
 
 function enterProjectName(e) {
 	e.which = e.which || e.keyCode;
 	if(e.which == 13) {
 		checkInputVal()
-		// addInputValToList()
 		clearInputField()
+		countListUl()
 	}
 }
 
 function addInputValToList() {
-	saveitemArray(input.value)
+	countItemArray(input.value)
 	li = document.createElement("li");
 	li.innerHTML = input.value
 	ul.appendChild(li)
 	addDraggablePropsToLi()
 }
 
-function addDraggablePropsToLi() {
-	li.setAttribute("id", input.length)
-	li.setAttribute("draggable", "true")
-	li.setAttribute("ondragstart", "dragStart(event)")
-}
 
 function clearInputField() {
 	input.value = ""
@@ -46,10 +44,32 @@ function checkInputVal() {
 	}	
 }
 
-function saveitemArray(item) {
+function countItemArray(item) {
 	items.push(item)
-	todoCounter.innerHTML = "" + items.length + " Projects" + "" 
+	projTot.innerHTML = "" + items.length + "<br> Projects" + "" 
 	console.log(items.length)
+}
+
+function countListUl() {
+	var todo = document.getElementById("todo-ul").getElementsByTagName("li"),
+	inprog = document.getElementById("inprogress-ul").getElementsByTagName("li"),
+	done = document.getElementById("done-ul").getElementsByTagName("li");
+	todoCounter.innerHTML = todo.length + " Projects"
+	inprogCounter.innerHTML = inprog.length + " Projects"
+	doneCounter.innerHTML = done.length + " Projects"
+}
+
+function sortList(id) {
+	 var ul = id;
+	 console.log(ul)
+  
+}
+
+//drag & drop events
+function addDraggablePropsToLi() {
+	li.setAttribute("id", input.length + input.value)
+	li.setAttribute("draggable", "true")
+	li.setAttribute("ondragstart", "dragStart(event)")
 }
 
 function dragStart(e) {
@@ -61,6 +81,7 @@ function drop(e) {
 	e.preventDefault()
 	var data = event.dataTransfer.getData("Text");
   event.target.appendChild(document.getElementById(data));
+	countListUl()
 }
 
 function allowDrop(e) {
